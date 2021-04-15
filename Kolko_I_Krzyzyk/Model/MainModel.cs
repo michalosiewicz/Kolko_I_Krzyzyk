20244acd8ded186b17f88f18177c962bce0dda0e
micos <micos@interia.pl>
2021-04-15 18:58:44 +0200

    Dodaj pliki projektów.

---
+++ Kolko_I_Krzyzyk/Model/MainModel.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Kolko_I_Krzyzyk.Model
+{
+    class MainModel
+    {
+        private char[] pola;
+        private char symbol = 'X';
+        
+        public MainModel()
+        {
+            pola = new char[9];
+        }
+
+        public char[] ruch_gracza(int indeks)
+        {
+            if (symbol == 'O')
+                symbol = 'X';
+            else
+                symbol = 'O';
+            pola[indeks] = symbol;
+            return pola;
+        }
+
+        public bool sprawdz_pole(int indeks)
+        {
+            if (pola[indeks] == 'O' || pola[indeks] == 'X')
+                return false;
+            else
+                return true;
+        }
+
+        public char sprawdz_gre()
+        {
+            int licznik = 0;
+            
+            for (int j = 0; j < 9; j += 3)
+            {
+                for (int i = j; i < j + 3; i++)
+                {
+                    if (pola[i] == symbol)
+                        licznik++;
+                    else
+                    {
+                        licznik = 0;
+                        break;
+                    }
+                }
+                if (licznik == 3)
+                    return symbol;
+            }
+            
+            for (int j = 0; j < 3; j ++)
+            {
+                for (int i = j; i < j + 7; i+=3)
+                {
+                    if (pola[i] == symbol)
+                        licznik++;
+                    else
+                    {
+                        licznik = 0;
+                        break;
+                    }
+                }
+                if (licznik == 3)
+                    return symbol;
+            }
+            
+            for (int i = 0; i < 9; i += 4)
+            {
+                if (pola[i] == symbol)
+                    licznik++;
+                else
+                {
+                    licznik = 0;
+                    break;
+                }
+            }
+            if (licznik == 3)
+                return symbol;
+            
+            for (int i = 2; i < 7; i += 2)
+            {
+                if (pola[i] == symbol)
+                    licznik++;
+                else
+                {
+                    licznik = 0;
+                    break;
+                }
+            }
+            if (licznik == 3)
+                return symbol;
+            return '0';
+        }
+    }
+}
---
+++ Kolko_I_Krzyzyk/ViewModel/MainViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Kolko_I_Krzyzyk.ViewModel
+{
+    using BaseClass;
+    using System.Windows;
+
+    class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private Model.MainModel mainModel=new Model.MainModel();
+
+        private char[] symbol;
+        public char[] Symbol
+        {
+            get { return symbol; }
+
+            private set
+            {
+                symbol = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Symbol)));
+
+                char winner=mainModel.sprawdz_gre();
+                if (winner == 'O' || winner == 'X')
+                {
+                    MessageBox.Show($"Wygrał gracz {winner}!", "Zwycięzca");
+                    Application.Current.Shutdown();
+                }
+
+            }
+        }
+
+
+        private ICommand wpisz;
+        public ICommand Wpisz
+        {
+            
+            get
+            {
+                return wpisz ?? (wpisz = new RelayCommand(
+                    p => { Symbol = mainModel.ruch_gracza(int.Parse(p.ToString())); },
+                    p =>  mainModel.sprawdz_pole(int.Parse(p.ToString()))
+                    ));
+            }
+        }
+
+    }
+}
